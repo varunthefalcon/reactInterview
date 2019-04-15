e5e12ad41c483d38c567473f778ec825833e890c
varun <s.varun1054@gmail.com>
2019-04-15 10:47:58 +0530

    updates

--- src/App.js
+++ src/App.js
@@ -2,39 +2,55 @@ import React, { Component, Fragment } from 'react';
 // import Header from './components/header';
 // import Footer from './components/footer';
 import JSONdata from './mockJSON.json'
-import mapIcon  from './assets/images/icon_map@2x.png'
-import footerLunch  from "./assets/images/tab_lunch@2x.png"
-import footerInternet  from "./assets/images/tab_internets@2x.png"
+import mapIcon from './assets/images/icon_map@2x.png'
+import footerLunch from "./assets/images/tab_lunch@2x.png"
+import footerInternet from "./assets/images/tab_internets@2x.png"
+
+
 class App extends Component {
+  constructor(props) {
+    super(props);
+    console.clear()
+    // console.log(JSONdata.res)
+    this.state = {
+      restaurantLists: JSONdata.restaurants
+    }
+  }
   render() {
+    console.log(this.state)
+    const { restaurantLists } = this.state;
     return (
       <Fragment>
         <header>
-        <h1 className="header_title fontBold">Lunch Tyme</h1>
-        <div className="pull-right">
-            <img src={mapIcon} className="header_map" alt="header_map"/>
-        </div>
-    </header>
-    <main>
-    <div className=" list_items " style={{backgroundImage : `url('http://sandbox.bottlerocketapps.com/BR_iOS_CodingExam_2015_Server/Images/hopdoddy.png')`}} >
-            <div className='list_itemBlock'>
-                <p className='item_title fontBold'> Restaurant Name </p>
-                <p className='item_category fontRegular'> Category Type </p>
+          <h1 className="header_title fontBold">Lunch Tyme</h1>
+          <div className="pull-right">
+            <img src={mapIcon} className="header_map" alt="header_map" />
+          </div>
+        </header>
+        <main>
+          {restaurantLists.map((item, index) => {
+            const { name, backgroundImageURL, category} = item
+          return <Fragment key={index}>
+            <div className=" list_items " style={{ backgroundImage: `url(${backgroundImageURL})` }} >
+              <div className='list_itemBlock'>
+                <p className='item_title fontBold'> {name} </p>
+                <p className='item_category fontRegular'> {category} </p>
+              </div>
             </div>
-        </div>
-    </main>
-    <footer>
-        <div className="footer_tab">
+          </Fragment>})}
+        </main>
+        <footer>
+          <div className="footer_tab">
             <div className="footer_tab_item">
-                <img src={footerLunch} alt='tab_lunch' />
-                <p className="fontRegular">lunch</p>
+              <img src={footerLunch} alt='tab_lunch' />
+              <p className="fontRegular">lunch</p>
             </div>
             <div className="footer_tab_item">
-                <img src={footerInternet} alt='tab_internets' />
-                <p className="fontRegular footer_tab_item_internets">internets</p>
+              <img src={footerInternet} alt='tab_internets' />
+              <p className="fontRegular footer_tab_item_internets">internets</p>
             </div>
-        </div>
-    </footer>
+          </div>
+        </footer>
       </Fragment>
     );
   }
